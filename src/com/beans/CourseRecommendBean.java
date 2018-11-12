7480e54f78dc72c33f30df129e47efe08f513607
Karthik C <ckarthik114@gmail.com>
2018-11-13 01:04:27 +0530

    Added Course Recommendation

---
+++ src/com/beans/CourseRecommendBean.java
@@ -0,0 +1,40 @@
+package com.beans;
+
+public class CourseRecommendBean {
+	private String isbn, title, edition, course;
+	private int stock;
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getEdition() {
+		return edition;
+	}
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+	
+}
