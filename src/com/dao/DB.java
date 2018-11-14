4aa16bf1f50273831a722c2fd485aad1ea1b5d3e
Karthik C <ckarthik114@gmail.com>
2018-11-14 21:11:42 +0530

    Removed Comment for Password

--- src/com/dao/DB.java
+++ src/com/dao/DB.java
@@ -7,7 +7,7 @@ public static Connection getCon(){
 	Connection con=null;
 	try{
 		Class.forName("com.mysql.jdbc.Driver");
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/library?autoReconnect=true&useSSL=false","root",/* Enter pass here */ "1");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/library?autoReconnect=true&useSSL=false","root","1");
 		
 	}catch(Exception e){System.out.println(e);}
 	return con;
